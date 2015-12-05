23fec66b11b76470c541108f1634b84e489ece08
Alexander Rosenberg Johansen <s145706@mnemosyne.imm.dtu.dk>
2015-12-05 09:59:07 +0100

    debugging

--- rnn_spn.py
+++ rnn_spn.py
@@ -147,15 +147,15 @@ l_transform1 = lasagne.layers.TransformerLayer(
     downsample_factor=args.downsample)
 
 # Second SPN loc
-l_pool0_loc2 = pool(l_transform1, pool_size=(2, 2))
-l_conv0_loc2 = conv(l_pool0_loc2, num_filters=20, filter_size=(3, 3),
+#l_pool0_loc2 = pool(l_transform1, pool_size=(2, 2))
+l_conv0_loc2 = conv(l_transform1, num_filters=20, filter_size=(3, 3),
                    name='l_conv0_loc', W=W_ini)
-l_pool1_loc2 = pool(l_conv0_loc2, pool_size=(2, 2))
-l_conv1_loc2 = conv(l_pool1_loc2, num_filters=20, filter_size=(3, 3),
-                   name='l_conv1_loc', W=W_ini)
-l_conv1_loc2 = lasagne.layers.DropoutLayer(l_conv1_loc2, p=sh_drp)
-l_pool2_loc2 = pool(l_conv1_loc2, pool_size=(2, 2))
-l_conv2_loc2 = conv(l_pool2_loc2, num_filters=20, filter_size=(3, 3),
+#l_pool1_loc2 = pool(l_conv0_loc2, pool_size=(2, 2))
+#l_conv1_loc2 = conv(l_pool1_loc2, num_filters=20, filter_size=(3, 3),
+#                   name='l_conv1_loc', W=W_ini)
+#l_conv1_loc2 = lasagne.layers.DropoutLayer(l_conv1_loc2, p=sh_drp)
+#l_pool2_loc2 = pool(l_conv1_loc2, pool_size=(2, 2))
+l_conv2_loc2 = conv(l_conv0_loc2, num_filters=20, filter_size=(3, 3),
                    name='l_conv2_loc', W=W_ini)
 
 l_repeat_loc2 = Repeat(l_conv2_loc2, n=num_steps)
